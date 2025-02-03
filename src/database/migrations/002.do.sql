dbd720069a5928e1e6ab03ee4301d2d2e93dec32
Bharat Sonwane <bharatrsonwane@gmail.com>
2025-02-03 21:50:44 +0530

    migration data insert lookup_type & lookup

---
+++ src/database/migrations/002.do.sql
@@ -0,0 +1,21 @@
+-- Insert data into lookup_type table
+INSERT INTO lookup_type (name, created_at, updated_at)
+VALUES
+  ('role', NOW(), NOW()),
+  ('chatType', NOW(), NOW())
+ON CONFLICT (name) DO NOTHING;
+
+-- Insert data into lookup table for 'role'
+INSERT INTO lookup (label, lookup_type_id, created_at, updated_at)
+VALUES
+  ('Super Admin', (SELECT id FROM lookup_type WHERE name = 'role'), NOW(), NOW()),
+  ('Admin', (SELECT id FROM lookup_type WHERE name = 'role'), NOW(), NOW()),
+  ('User', (SELECT id FROM lookup_type WHERE name = 'role'), NOW(), NOW())
+ON CONFLICT (lookup_type_id, label) DO NOTHING;
+
+-- Insert data into lookup table for 'chatType'
+INSERT INTO lookup (label, lookup_type_id, created_at, updated_at)
+VALUES
+  ('1:1 Chat', (SELECT id FROM lookup_type WHERE name = 'chatType'), NOW(), NOW()),
+  ('Group Chat', (SELECT id FROM lookup_type WHERE name = 'chatType'), NOW(), NOW())
+ON CONFLICT (lookup_type_id, label) DO NOTHING;
